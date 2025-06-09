88236b72af037d9c725740e29cc716c4ac2deeb3
MIRAGZAMOV <miragzamov24@gmail.com>
2025-06-09 20:41:58 +0500

    more clear dotenv usage

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import { GoogleGenAI } from "@google/genai";
-import dotenv from "dotenv";
+import dotenv from "dotenv/config";
 
-dotenv.config();
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
 const response = await ai.models.generateContent({
